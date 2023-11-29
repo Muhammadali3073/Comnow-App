91f2f9e7f36dabcb2e549f818d309d776e78a671
Muhammad Ali Nawaz <78873323+Muhammadali3073@users.noreply.github.com>
2023-11-29 15:16:38 +0500

    Start App 29-11-23

---
+++ README.md
@@ -0,0 +1,9 @@
+# comnow
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+
---
+++ android/app/src/main/kotlin/com/example/comnow/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.comnow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
